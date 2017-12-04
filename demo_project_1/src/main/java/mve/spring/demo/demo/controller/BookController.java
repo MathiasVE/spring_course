1a0f2184b3e88f09c569a48fe573e71bae60a1ca
Mathias Ver Elst <mathias.ver.elst@gmail.com>
2017-12-04 10:00:09 +0100

    Add book controller

---
+++ demo_project_1/src/main/java/mve/spring/demo/demo/controller/BookController.java
@@ -0,0 +1,21 @@
+package mve.spring.demo.demo.controller;
+
+import mve.spring.demo.demo.model.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+  @Autowired
+  private BookRepository bookRepository;
+
+  @RequestMapping("/books")
+  public String getBooks(Model model) {
+    model.addAttribute("books", bookRepository.findAll());
+
+    return "books";
+  }
+}
