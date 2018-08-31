166e041d32f52302e2398f26d6d3e3c224ca2559
Mathias Ver Elst <mathias.ver.elst@gmail.com>
2018-08-31 19:41:08 +0200

    Added bootstrap 4

--- demo_project_recipes/src/main/java/mve/demo/recipe/controllers/IndexController.java
+++ demo_project_recipes/src/main/java/mve/demo/recipe/controllers/IndexController.java
@@ -1,18 +1,9 @@
 package mve.demo.recipe.controllers;
 
-import mve.demo.recipe.domain.Category;
-import mve.demo.recipe.domain.Recipe;
-import mve.demo.recipe.domain.UnitOfMeasure;
-import mve.demo.recipe.repositories.CategoryRepository;
-import mve.demo.recipe.repositories.RecipeRepository;
-import mve.demo.recipe.repositories.UnitOfMeasureRepository;
 import mve.demo.recipe.services.RecipeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
